8738acbeefd2702a4656bcadeacc0e34dd7a38be
Heorhi Parynau <Heorhi_Parynau@epam.com>
2025-08-25 17:41:33 +0200

    add a new suite

---
+++ TestSuitesExamples/TestSuiteExamples/ExampleSuiteCTests.cs
@@ -0,0 +1,21 @@
+﻿namespace TestSuitesExamples;
+
+[Collection("SuiteA")]
+public class ExampleSuiteCTests
+{
+    [Fact(DisplayName = "C: new Test suite")]
+    [Trait("Suite", "C")]
+    public async Task TestMethod1()
+    {
+        await Task.Delay(100);
+        Assert.True(true);
+    }
+    
+    [Fact(DisplayName = "C: Exclude")]
+    [Trait("Suite", "C")]
+    public async Task TestMethod2()
+    {
+        await Task.Delay(100);
+        Assert.True(true);
+    }
+}
